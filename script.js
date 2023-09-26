0dd91061eb759e561b9456642fa7cca52ca9274d
Natalia <natalia.levinta@amdaris.com>
2023-09-26 10:57:09 +0300

    Added complete logic

--- script.js
+++ script.js
@@ -1,31 +1,56 @@
 
-const progressBar= document.querySelector('.progress_steps-bar');
+const progressBar = document.querySelector('.progress_steps-bar');
 const nextButton = document.querySelector('.next');
 const prevButton = document.querySelector('.prev');
 const circles = document.querySelectorAll('.progress_steps-circle');
-progressBar.style.width = '33%';
+
 
 let counter = 1;
 
 const nextHandler = () => {
-    counter ++;
+    counter++;
 
-    if( counter >= circles.length) {
+    if (counter >= circles.length) {
         counter = 4;
     }
     console.log(counter);
 }
 
-const prvHandler = () => {
-    counter --;
+const prevHandler = () => {
+    counter--;
 
-    if(counter <=1) {
+    if (counter <= 1) {
         counter = 1;
     }
     console.log(counter);
 }
 
+const updateCircle = () => {
+    circles.forEach((item, index) => {
+        if (index + 1 <= counter) {
+            item.classList.add('active');
+        } else if (index + 1 > counter) {
+            item.classList.remove('active');
+        }
+    })
+}
+
+const updateProgress = () => {
+    circles.forEach((item, index) => {
+        const activeCircles = document.querySelectorAll('.active');
+        const progressWidth = (activeCircles.length - 1) / (circles.length - 1) * 100;
+
+        if (progressWidth <= 100) {
+            progressBar.style.width = progressWidth + '%';
+        }
+    })
+}
+
+
 
 nextButton.addEventListener('click', nextHandler);
-prevButton.addEventListener('click', prvHandler);
-
+nextButton.addEventListener('click', updateCircle);
+nextButton.addEventListener('click', updateProgress);
+prevButton.addEventListener('click', prevHandler);
+prevButton.addEventListener('click', updateCircle);
+prevButton.addEventListener('click', updateProgress);
